a0284419c8b165070224dfce68f11b5258aee65d
Abhishek Singh <singhabhishek111213@gmail.com>
2024-02-27 17:17:04 +0530

    Added ui changes in shopping lists

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,31 +1,30 @@
 import Item from "../Item/Item";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import "./ItemList.css";
 
 function ItemList({ shoppingItems }) {
   return (
-    <>
-      <div className="shopping-items-wrapper">
-        {shoppingItems &&
-          shoppingItems.map((item) => {
-            return (
-              <div key={item.id}>
-                <div className="change-quantity add-item">
-                  <FontAwesomeIcon icon={faPlus} />
-                </div>
-                <Item
-                  itemName={item.name}
-                  key={item.id}
-                  quantity={item.quantity}
-                />
-                <div className="change-quantity remove-item">
-                  <FontAwesomeIcon icon={faMinus} />
-                </div>
+    <div className="shopping-items-wrapper">
+      {shoppingItems &&
+        shoppingItems.map((item) => {
+          return (
+            <div key={item.id} className="items-list">
+              <div className="change-quantity add-item">
+                <FontAwesomeIcon icon={faPlus} />
               </div>
-            );
-          })}
-      </div>
-    </>
+              <Item
+                itemName={item.name}
+                key={item.id}
+                quantity={item.quantity}
+              />
+              <div className="change-quantity remove-item">
+                <FontAwesomeIcon icon={faMinus} />
+              </div>
+            </div>
+          );
+        })}
+    </div>
   );
 }
 
